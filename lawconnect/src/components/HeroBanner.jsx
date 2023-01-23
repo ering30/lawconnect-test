bd2061f06bb8ecf16145f9ac83eb51006c6dd4c8
Erin Gonthier <eringonthier@gmail.com>
2023-01-23 23:26:44 +1100

    second commit

--- lawconnect/src/components/HeroBanner.jsx
+++ lawconnect/src/components/HeroBanner.jsx
@@ -1,32 +1,55 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import '../../src/App.css';
 import AccountButton from './AccountButton';
 
+function useWindowDimensions() {
+    const [width, setWidth] = useState(window.innerWidth);
+
+    const updateWidth = () => {
+        setWidth(window.innerWidth);
+    };
+
+    useEffect(() => {
+        window.addEventListener("resize", updateWidth);
+        return () => window.removeEventListener("resize", updateWidth);
+    });
+
+    return {
+        width
+    }
+}
+
 export default function HeroBanner() {
+    const { width } = useWindowDimensions()
     return (
         <div class="heroDiv">
-            <div class="heroText">
-                <div class="contentContainer">
+                <div class="heroContentContainer">
                     <div class="hero-flex-item-1">
                         <h1>The place to work with your lawyer</h1>
-                        <h4>
+                        <p class="heroText">
                             Your lawyer uses LawConnect to share information with you about your legal case 
                             in a private, convenient and secure way.
-                        </h4>
+                        </p>
                         <AccountButton />
                     </div>
                     <div class="hero-flex-item-2" style={{alignItems: "center"}}>
-                        <div class="circleDiv">
-                            <img 
-                                class="heroImage" 
-                                src='assets/images/lc-hero-desktop@2x.png' 
+                        {width > 1013 ? 
+                            <div class="circleDiv">
+                                <img 
+                                    class="heroImage" 
+                                    src='assets/images/lc-hero-desktop@2x.png' 
+                                    alt='Graphic showing LawConnect user interface'
+                                />
+                            </div>
+                            : <img 
+                                class="heroImage-mobile" 
+                                src='assets/images/lc-hero-mobile@2x.png' 
                                 alt='Graphic showing LawConnect user interface'
-                            />
-                        </div>
+                                />
+                        }
+                        
                     </div>
-                
                 </div>
-            </div>
         </div>
     )
 }
--- lawconnect/src/components/Navbar.jsx
+++ lawconnect/src/components/Navbar.jsx
@@ -1,22 +1,46 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import AccountButton from './AccountButton';
 import '../../src/App.css';
 import NavButton from './NavButton';
 
+function useWindowDimensions() {
+    const [width, setWidth] = useState(window.innerWidth);
+
+    const updateWidth = () => {
+        setWidth(window.innerWidth);
+    };
+
+    useEffect(() => {
+        window.addEventListener("resize", updateWidth);
+        return () => window.removeEventListener("resize", updateWidth);
+    });
+
+    return {
+        width
+    }
+}
+
 export default function Navbar() {
+    const { width } = useWindowDimensions()
     return (
         <div class="navbarContainer">
             <div class="toolbar">
                 <a href='/'><img class="logoButton" src='assets/logos/LawConnect-logo-reverse-RGB.png' alt='LawConnect Logo'/></a>
-                <div class="centreNavGroup">
+                {width > 1013 ? 
+                <>
+                    <div class="centreNavGroup">
                     <NavButton buttonText="For People"/>
                     <NavButton buttonText="For Businesses"/>
                     <NavButton buttonText="Contact"/>
-                </div>
-                <div class="navMenuDiv">
-                    <NavButton buttonText="Login" />
-                    <AccountButton buttonText="Create Free Account"/>
-                </div>
+                    </div>
+                    <div class="navMenuDiv">
+                        <NavButton buttonText="Login" />
+                        <AccountButton buttonText="Create Free Account"/>
+                    </div>
+                </>
+                : <span style={{color: '#ffffff', fontSize:'1.5em' }}>
+                    <i class="fa-solid fa-bars"></i>
+                    </span> }
             </div>
         </div>
     )
